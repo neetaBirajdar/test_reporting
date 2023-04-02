618e6b5e0b1cc797aa75edc52bf2ec2c5b6a1722
Neeta Birajdar <birajdarneeta@gmail.com>
2023-04-02 14:33:03 +0200

    fix: Print result

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -6,42 +6,42 @@ import pytest
 def test_addition_with_valid_values(calculator, value_1, value_2):
     expected_result = value_1 + value_2
     assert calculator.addition(value_1, value_2) == expected_result
-    print(f"value_1: {value_1}\nvalue_2: {value_2}\nRESULT: {expected_result}")
+    print(f"Addition:\nvalue_1: {value_1}\nvalue_2: {value_2}\nRESULT: {expected_result}")
 
 
 @with_invalid_values
 def test_addition_with_invalid_values(calculator, value_1, value_2):
     result = calculator.addition(value_1, value_2)
     assert "Failed" in result
-    print(f"value_1: {value_1}\nvalue_2: {value_2}\nRESULT: {result}")
+    print(f"Addition:\nvalue_1: {value_1}\nvalue_2: {value_2}\nRESULT: {result}")
 
 
 @with_valid_values
 def test_subtraction_with_valid_values(calculator, value_1, value_2):
     expected_result = value_1 - value_2
     assert calculator.subtraction(value_1, value_2) == expected_result
-    print(f"value_1: {value_1}\nvalue_2: {value_2}\nRESULT: {expected_result}")
+    print(f"Subtraction:\nvalue_1: {value_1}\nvalue_2: {value_2}\nRESULT: {expected_result}")
 
 
 @with_invalid_values
 def test_subtraction_with_invalid_values(calculator, value_1, value_2):
     result = calculator.subtraction(value_1, value_2)
     assert "Failed" in result
-    print(f"value_1: {value_1}\nvalue_2: {value_2}\nRESULT: {result}")
+    print(f"Subtraction:\nvalue_1: {value_1}\nvalue_2: {value_2}\nRESULT: {result}")
 
 
 @with_valid_values
 def test_division_with_valid_values(calculator, value_1, value_2):
     expected_result = value_1 / value_2
     assert calculator.division(value_1, value_2) == expected_result
-    print(f"value_1: {value_1}\nvalue_2: {value_2}\nRESULT: {expected_result}")
+    print(f"Division:\nvalue_1: {value_1}\nvalue_2: {value_2}\nRESULT: {expected_result}")
 
 
 @with_invalid_values
 def test_division_with_invalid_values(calculator, value_1, value_2):
     result = calculator.division(value_1, value_2)
     assert "Failed" in result
-    print(f"value_1: {value_1}\nvalue_2: {value_2}\nRESULT: {result}")
+    print(f"Division:\nvalue_1: {value_1}\nvalue_2: {value_2}\nRESULT: {result}")
 
 
 def test_division_with_divide_by_zero(calculator):
@@ -49,18 +49,18 @@ def test_division_with_divide_by_zero(calculator):
     value_2 = 0
     result = calculator.division(value_1, value_2)
     assert "Failed" in result
-    print(f"value_1: {value_1}\nvalue_2: {value_2}\nRESULT: {result}")
+    print(f"Division:\nvalue_1: {value_1}\nvalue_2: {value_2}\nRESULT: {result}")
 
 
 @with_valid_values
 def test_multiplication_with_valid_values(calculator, value_1, value_2):
     expected_result = value_1 * value_2
     assert calculator.multiplication(value_1, value_2) == expected_result
-    print(f"value_1: {value_1}\nvalue_2: {value_2}\nRESULT: {expected_result}")
+    print(f"Multiplication:\nvalue_1: {value_1}\nvalue_2: {value_2}\nRESULT: {expected_result}")
 
 
 @with_invalid_values
 def test_multiplication_with_invalid_values(calculator, value_1, value_2):
     result = calculator.multiplication(value_1, value_2)
     assert "Failed" in result
-    print(f"value_1: {value_1}\nvalue_2: {value_2}\nRESULT: {result}")
+    print(f"Multiplication:\nvalue_1: {value_1}\nvalue_2: {value_2}\nRESULT: {result}")
