e9c068c4b31470a2f80cb5d7bc1a31c0b1db150c
Neeta Birajdar <birajdarneeta@gmail.com>
2023-04-02 13:52:55 +0200

    add: test for subtraction + common_params

--- test/conftest.py
+++ test/conftest.py
@@ -7,12 +7,12 @@ def calculator() -> Calculator:
     return Calculator()
 
 
-with_valid_addition_values = pytest.mark.parametrize(
-    "value_1, value_2, result",
-    [[10, 20, 30], [-10, 20, 10], [10, 20.5, 30.5], [10.20, 20.30, 30.50]],
+with_valid_values = pytest.mark.parametrize(
+    "value_1, value_2",
+    [[10, 20], [-10, 20], [10, 20.5], [10.20, 20.30], [0, 0], [-30, -30]],
 )
 
-with_invalid_addition_values = pytest.mark.parametrize(
+with_invalid_values = pytest.mark.parametrize(
     "value_1, value_2",
     [
         [0, "a"],
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,14 +1,29 @@
-from .conftest import with_valid_addition_values, with_invalid_addition_values
+from .conftest import with_invalid_values, with_valid_values
 
 
-@with_valid_addition_values
-def test_addition_with_valid_values(calculator, value_1, value_2, result):
-    assert calculator.addition(value_1, value_2) == result
+@with_valid_values
+def test_addition_with_valid_values(calculator, value_1, value_2):
+    expected_result = value_1 + value_2
+    assert calculator.addition(value_1, value_2) == expected_result
+    print(f"value_1: {value_1}\nvalue_2: {value_2}\nRESULT: {expected_result}")
+
+
+@with_invalid_values
+def test_addition_with_invalid_values(calculator, value_1, value_2):
+    result = calculator.addition(value_1, value_2)
+    assert "Failed" in result
     print(f"value_1: {value_1}\nvalue_2: {value_2}\nRESULT: {result}")
 
 
-@with_invalid_addition_values
-def test_addition_with_invalid_values(calculator, value_1, value_2):
-    result = calculator.addition(value_1, value_2)
+@with_valid_values
+def test_subtraction_with_valid_values(calculator, value_1, value_2):
+    expected_result = value_1 - value_2
+    assert calculator.subtraction(value_1, value_2) == expected_result
+    print(f"value_1: {value_1}\nvalue_2: {value_2}\nRESULT: {expected_result}")
+
+
+@with_invalid_values
+def test_subtraction_with_invalid_values(calculator, value_1, value_2):
+    result = calculator.subtraction(value_1, value_2)
     assert "Failed" in result
     print(f"value_1: {value_1}\nvalue_2: {value_2}\nRESULT: {result}")
